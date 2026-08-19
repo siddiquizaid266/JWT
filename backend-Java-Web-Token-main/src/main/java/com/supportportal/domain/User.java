9e29fafb7954bfc69342f180b8ec681ce8b918a2
siddiquizaid266 <siddiquizaid266@gmail.com>
2026-08-19 22:30:51 +0300

    backend frontend implementation

---
+++ backend-Java-Web-Token-main/src/main/java/com/supportportal/domain/User.java
@@ -0,0 +1,41 @@
+package com.supportportal.domain;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String role; //ROLE_USER{ read, edit }, ROLE_ADMIN {delete}
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+
+   
+}
